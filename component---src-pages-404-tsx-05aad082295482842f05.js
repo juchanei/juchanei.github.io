729eec1c6f18c068ed2c2946a863567e843b085c
LEE JUCHAN <juchanei@naver.com>
2021-01-19 23:42:11 +0900

    Updates

---
+++ component---src-pages-404-tsx-05aad082295482842f05.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{"i6+/":function(n,e,t){"use strict";t.r(e);var a=t("q1tI"),o=t.n(a);e.default=function(){return o.a.createElement("main",null,o.a.createElement("p",null,"Sorry, page not found!"))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-05aad082295482842f05.js.map
