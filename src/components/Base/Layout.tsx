808be5a45f6e86bf82618b4018ee7439c18adbfc
LEE JUCHAN <juchanei@naver.com>
2021-01-20 03:12:51 +0900

    refactor: Replace hard coding Navigatino Links to data structure

--- src/components/Base/Layout.tsx
+++ src/components/Base/Layout.tsx
@@ -2,22 +2,34 @@ import React from 'react';
 
 import GithubLink from '@/components/Base/GithubLink';
 
+interface NavLinkOption {
+    path: string;
+    name: string;
+    target?: string;
+}
+
+const links: NavLinkOption[] = [
+    { path: '/about', name: 'About' },
+    { path: '/blog', name: 'Blog' },
+    {
+        path: 'https://github.com/juchanei',
+        name: 'Github',
+        target: 'blank',
+    },
+];
+
 const Layout: React.FC = ({ children }) => (
     <main>
         <h1>juchanei&apos;s blog</h1>
         <header>
             <ul>
-                <li>
-                    <a href="/about">About</a>
-                </li>
-                <li>
-                    <a href="/blog">Blog</a>
-                </li>
-                <li>
-                    <a href="https://github.com/juchanei" target="blank">
-                        Github
-                    </a>
-                </li>
+                {links.map(({ path, name, target }) => (
+                    <li>
+                        <a href={path} target={target}>
+                            {name}
+                        </a>
+                    </li>
+                ))}
             </ul>
         </header>
         <article>{children}</article>
